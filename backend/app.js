ff8cfaaec3bccbbf2ef1a1ea32f92ad003233535
Araf-Khatri <arafkhatri0@gmail.com>
2025-08-13 12:36:25 +0530

    update: cors origin to be dynamic

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
 const todoRouter = require("./Routes/todo-routes");
 const userRouter = require("./Routes/user-routes");
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 
-app.use("*", cors({
-  origin: "https://todo-list-araf.netlify.app"
-}));
-
+app.use(
+  "*",
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 app.use("/api/todo", todoRouter);
 app.use("/api/user", userRouter);
